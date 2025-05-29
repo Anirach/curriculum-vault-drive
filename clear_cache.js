20cdb89484aff1432eafd6d05158fb73ceeaad02
Anirach Mingkhwan <anirach.m@fitm.kmutnb.ac.th>
2025-05-29 12:50:22 +0700

    Remove Unuse file

--- clear_cache.js
+++
@@ -1,6 +0,0 @@
-
-// Clear all cached data
-localStorage.clear();
-console.log('All localStorage data cleared');
-window.location.reload();
-
--- src/components/SimpleCurriculumApp.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const SimpleCurriculumApp: React.FC = () => {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      {/* Removed PublicDashboard component */}
-    </div>
-  );
-};
